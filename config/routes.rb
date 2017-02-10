8b83e24e604391f9fa3d676ebd68cd7899cbb0ee
Will <wmitbrodt@gmail.com>
2017-02-10 15:17:06 -0800

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'blog/index'
 
-  get 'blog/post'
+  get '/' => 'blog#index'
+
+  get '/show' => 'blog#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
