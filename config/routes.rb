f08ba76c2ecd116236d0da080e0972250261cbf4
Will <wmitbrodt@gmail.com>
2017-02-10 14:59:39 -0800

    Add a blog controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'blog/index'
+
+  get 'blog/post'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
