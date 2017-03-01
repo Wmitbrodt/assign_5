5a65d109609eaab4ff70cc7504cdfa63c6e4b9fe
Will <wmitbrodt@gmail.com>
2017-02-28 18:45:54 -0800

    fixed in comments create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,19 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, except: [:create]
+  before_action :authenticate_user!, except: [:show, :index]
 
 
   def create
     comment_params   = params.require(:comment).permit(:body)
     @comment         = Comment.new comment_params
     @post            = Post.find params[:post_id]
-
-    @comment.post    = @post
     @comment.user = current_user
-    @comments = Comment.where(post_id: params[:post_id])
+    @comment.post    = @post
+
 
     if @comment.save
       redirect_to post_path(params[:post_id]), notice: 'Comment was successfully created'
     else
-      flash[:alert] = 'You need to fix errors'
+      flash.now[:alert] = 'You need to fix errors'
       render '/posts/show'
     end
   end
